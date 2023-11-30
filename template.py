d1043516c4f6db6516812c11e0a8e4afaf12b1c9
Ryan Echols <rkechols21@gmail.com>
2023-11-29 23:59:40 -0700

    basic python template/setup

---
+++ template.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from pprint import pprint
+from typing import Any
+
+Input = str  # feel free to change per-problem; whatever structure is easiest
+
+INPUT_FILE_PATH = Path("input.txt")
+
+
+def read() -> Input:
+    with open(INPUT_FILE_PATH, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def solve(input_: Input) -> Any:
+    pass
+
+
+def main():
+    input_ = read()
+    answer = solve(input_)
+    pprint(answer)
+
+
+if __name__ == "__main__":
+    main()
