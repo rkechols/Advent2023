e71d6309ee322cd7aecf602d35eadc2646b2af2d
Ryan Echols <rkechols21@gmail.com>
2023-11-30 22:26:03 -0700

    day 1 part 2

--- day01.py
+++ day01.py
@@ -13,7 +13,7 @@ def read_input() -> Input:
         return f.readlines()
 
 
-def solve(input_: Input) -> Any:
+def solve1(input_: Input) -> Any:
     total = 0
     for line in input_:
         for c in line:
@@ -33,9 +33,47 @@ def solve(input_: Input) -> Any:
     return total
 
 
+def to_digit(num: str) -> str:
+    if num.isdigit():
+        return num
+    match num:
+        case "one":
+            return "1"
+        case "two":
+            return "2"
+        case "three":
+            return "3"
+        case "four":
+            return "4"
+        case "five":
+            return "5"
+        case "six":
+            return "6"
+        case "seven":
+            return "7"
+        case "eight":
+            return "8"
+        case "nine":
+            return "9"
+        case _:
+            raise ValueError(num)
+
+
+def solve2(input_: Input) -> Any:
+    total = 0
+    for line in input_:
+        first = re.search(r"\d|one|two|three|four|five|six|seven|eight|nine", line).group(0)
+        last = re.search(r"\d|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno", line[::-1]).group(0)[::-1]
+        number = int(f"{to_digit(first)}{to_digit(last)}")
+        total += number
+    return total
+
+
 def main():
     input_ = read_input()
-    answer = solve(input_)
+    answer = solve1(input_)
+    pprint(answer)
+    answer = solve2(input_)
     pprint(answer)
 
 
