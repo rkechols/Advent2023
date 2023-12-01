9b3d7c764f3da32bf6f1ef4cfd84d2a6b5cf1c95
Ryan Echols <rkechols21@gmail.com>
2023-11-30 22:44:22 -0700

    day 1 - cleanup

--- day01.py
+++ day01.py
@@ -1,69 +1,59 @@
-from pathlib import Path
 import re
+from pathlib import Path
 from pprint import pprint
 from typing import Any
 
-Input = list[str]  # feel free to change per-problem; whatever structure is easiest
+Input = list[str]
 
 INPUT_FILE_PATH = Path("input.txt")
 
 
 def read_input() -> Input:
     with open(INPUT_FILE_PATH, "r", encoding="utf-8") as f:
-        return f.readlines()
+        return [
+            line_
+            for line in f
+            if (line_ := line.strip()) != ""
+        ]
 
 
 def solve1(input_: Input) -> Any:
     total = 0
     for line in input_:
-        for c in line:
-            if c.isdigit():
-                first = c
-                break
-        else:
-            raise ValueError(line)
-        for c in line[::-1]:
-            if c.isdigit():
-                last = c
-                break
-        else:
-            raise ValueError(line)
+        first = re.search(r"\d", line).group(0)
+        last = re.search(r"\d", line[::-1]).group(0)
         number = int(f"{first}{last}")
         total += number
     return total
 
 
+DIGITS_BY_NAME = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+
 def to_digit(num: str) -> str:
     if num.isdigit():
         return num
-    match num:
-        case "one":
-            return "1"
-        case "two":
-            return "2"
-        case "three":
-            return "3"
-        case "four":
-            return "4"
-        case "five":
-            return "5"
-        case "six":
-            return "6"
-        case "seven":
-            return "7"
-        case "eight":
-            return "8"
-        case "nine":
-            return "9"
-        case _:
-            raise ValueError(num)
+    return DIGITS_BY_NAME[num]
 
 
 def solve2(input_: Input) -> Any:
+    union_digit_names = "|".join(DIGITS_BY_NAME.keys())
+    re_digits_forward = re.compile(f"\\d|{union_digit_names}")
+    re_digits_backward = re.compile(f"\\d|{union_digit_names[::-1]}")
     total = 0
     for line in input_:
-        first = re.search(r"\d|one|two|three|four|five|six|seven|eight|nine", line).group(0)
-        last = re.search(r"\d|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno", line[::-1]).group(0)[::-1]
+        first = re_digits_forward.search(line).group(0)
+        last = re_digits_backward.search(line[::-1]).group(0)[::-1]
         number = int(f"{to_digit(first)}{to_digit(last)}")
         total += number
     return total
