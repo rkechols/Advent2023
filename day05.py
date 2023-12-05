02d6d1dd7ec06f4229098e283b2345a57bc42694
Ryan Echols <rkechols21@gmail.com>
2023-12-05 01:19:56 -0700

    introduce type alias 'Section'

--- day05.py
+++ day05.py
@@ -18,7 +18,7 @@ RangeTransform = NamedTuple("RangeTransform", [
 @dataclass
 class Map:
     name: str
-    ranges: list[RangeTransform]
+    range_transforms: list[RangeTransform]
 
 
 @dataclass
@@ -41,7 +41,7 @@ def read_input() -> Input:
             for line in lines[1:]
             if line.strip()
         ]
-        maps.append(Map(name=name, ranges=ranges))
+        maps.append(Map(name=name, range_transforms=ranges))
     return Input(seeds=seeds, maps=maps)
 
 
@@ -49,7 +49,7 @@ def solve1(input_: Input) -> int:
     current_nums = set(input_.seeds)
     for map_ in input_.maps:
         new_nums = set()
-        for r in map_.ranges:
+        for r in map_.range_transforms:
             range_in = range(r.start_in, r.start_in + r.n)
             shift = r.start_out - r.start_in
             to_discard = set()
@@ -60,81 +60,80 @@ def solve1(input_: Input) -> int:
             current_nums -= to_discard
             if len(current_nums) == 0:
                 break
-        for num_no_match in current_nums:
-            new_nums.add(num_no_match)
+        new_nums.update(current_nums)
         current_nums = new_nums
     return min(current_nums)
 
 
-def _simplify_ranges(ranges: list[tuple[int, int]]) -> list[tuple[int, int]]:
+Section = tuple[int, int]  # tuple of form (start, end) where BOTH values are INCLUSIVE
+
+
+def _simplify_sections(sections: list[Section]) -> list[Section]:
     i = 0
-    while i + 1 < len(ranges):
-        (low_start, low_end), (high_start, high_end) = ranges[i:(i + 2)]
+    while i + 1 < len(sections):
+        (low_start, low_end), (high_start, high_end) = sections[i:(i + 2)]
         assert low_start <= high_start
         if low_end + 1 >= high_start:
-            mashed_range = (min(low_start, high_start), max(low_end, high_end))
-            ranges = ranges[:i] + [mashed_range] + ranges[(i + 2):]
+            mashed_section = (min(low_start, high_start), max(low_end, high_end))
+            sections = sections[:i] + [mashed_section] + sections[(i + 2):]
         else:
             i += 1
-    return ranges
+    return sections
 
 
-def _range_overlap(r_query: tuple[int, int], r_key: tuple[int, int]) -> tuple[
-    Optional[tuple[int, int]],
-    list[tuple[int, int]],
-]:
-    rq_low, rq_high = r_query
-    assert rq_low <= rq_high
-    rk_low, rk_high = r_key
-    assert rk_low <= rk_high
-    # disjoint ranges
-    if rq_high < rk_low or rk_high < rq_low:
-        return None, [r_query]
-    # overlapping ranges
+def _range_overlap(sec_query: Section, sec_key: Section) -> tuple[Optional[Section], list[Section]]:
+    sq_low, sq_high = sec_query
+    assert sq_low <= sq_high
+    sk_low, sk_high = sec_key
+    assert sk_low <= sk_high
+    # disjoint sections
+    if sq_high < sk_low or sk_high < sq_low:
+        return None, [sec_query]
+    # overlapping sections
     # fudge things to avoid boundaries perfectly overlapping
-    if rq_low == rk_low:
-        rk_low -= 1
-    if rq_high == rk_high:
-        rk_high += 1
+    if sq_low == sk_low:
+        sk_low -= 1
+    if sq_high == sk_high:
+        sk_high += 1
     # figure out the actual overlap
-    if rq_low < rk_low <= rq_high < rk_high:
-        return (rk_low, rq_high), [(rq_low, rk_low - 1)]
-    elif rq_low < rk_low <= rk_high < rq_high:
-        return (rk_low, rk_high), [(rq_low, rk_low - 1), (rk_high + 1, rq_high)]
-    elif rk_low < rq_low <= rq_high < rk_high:
-        return (rq_low, rq_high), []
-    elif rk_low < rq_low <= rk_high < rq_high:
-        return (rq_low, rk_high), [(rk_high + 1, rq_high)]
+    if sq_low < sk_low <= sq_high < sk_high:
+        return (sk_low, sq_high), [(sq_low, sk_low - 1)]
+    elif sq_low < sk_low <= sk_high < sq_high:
+        return (sk_low, sk_high), [(sq_low, sk_low - 1), (sk_high + 1, sq_high)]
+    elif sk_low < sq_low <= sq_high < sk_high:
+        return (sq_low, sq_high), []
+    elif sk_low < sq_low <= sk_high < sq_high:
+        return (sq_low, sk_high), [(sk_high + 1, sq_high)]
     else:
-        raise ValueError(f"{(rq_low, rq_high)=}, {(rk_low, rk_high)=}")
+        raise ValueError(f"{(sq_low, sq_high)=}, {(sk_low, sk_high)=}")
 
 
 def solve2(input_: Input) -> int:
-    input_ranges = _simplify_ranges(sorted(
+    input_sections = _simplify_sections(sorted(
         (start, start + n - 1)
         for start, n in zip(input_.seeds[::2], input_.seeds[1::2])
     ))
     for map_ in input_.maps:
-        output_ranges = []
-        for r in map_.ranges:
-            transform_range = (r.start_in, r.start_in + r.n - 1)
+        output_sections = []
+        for r in map_.range_transforms:
+            transforming_sec = (r.start_in, r.start_in + r.n - 1)
             shift = r.start_out - r.start_in
             i = 0
-            while i < len(input_ranges):
-                input_range = input_ranges[i]
-                overlap, leftovers = _range_overlap(input_range, transform_range)
+            while i < len(input_sections):
+                input_sec = input_sections[i]
+                overlap, leftovers = _range_overlap(input_sec, transforming_sec)
                 if overlap is not None:
                     overlap_shifted = (overlap[0] + shift, overlap[1] + shift)
-                    bisect.insort(output_ranges, overlap_shifted)
-                    input_ranges = input_ranges[:i] + leftovers + input_ranges[(i + 1):]
+                    bisect.insort(output_sections, overlap_shifted)
+                    input_sections = input_sections[:i] + leftovers + input_sections[(i + 1):]
                 else:
                     i += 1
-            if len(input_ranges) == 0:
+            if len(input_sections) == 0:
                 break
-        for input_range in input_ranges:  # ranges that go unchanged
-            bisect.insort(output_ranges, input_range)
-        input_ranges = _simplify_ranges(output_ranges)
-    return min(low for low, _ in input_ranges)
+        for input_sec in input_sections:  # sections that go unchanged
+            bisect.insort(output_sections, input_sec)
+        input_sections = _simplify_sections(output_sections)
+    return min(low for low, _ in input_sections)
 
 
 def main():
