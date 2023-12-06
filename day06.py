ec018b5e683277bdc8974e92844b19f4bbd418a9
Ryan Echols <rkechols21@gmail.com>
2023-12-05 23:51:53 -0700

    day 6 part 2

--- day06.py
+++ day06.py
@@ -1,9 +1,8 @@
 import math
 import re
+from functools import cache
 from pathlib import Path
 from pprint import pprint
-from typing import Any
-import bisect
 
 Input = list[tuple[int, int]]
 
@@ -19,6 +18,7 @@ def read_input() -> Input:
     return list(zip(times, distances_to_beat))
 
 
+@cache
 def _evaluate_race(time_total: int, time_held: int) -> int:
     assert 0 <= time_held <= time_total
     speed = time_held
@@ -27,7 +27,7 @@ def _evaluate_race(time_total: int, time_held: int) -> int:
     return distance
 
 
-def solve(input_: Input) -> int:
+def solve1(input_: Input) -> int:
     ways = []
     for time_total, distance_to_beat in input_:
         for time_held in range(1, time_total):
@@ -49,9 +49,91 @@ def solve(input_: Input) -> int:
     return math.prod(ways)
 
 
+def _find_peak(t_min: int, t_max: int, *, t_total: int) -> int:
+    assert 0 <= t_min <= t_max <= t_total
+    n_to_test = 1 + t_max - t_min
+    if n_to_test == 1:
+        return t_min
+    if n_to_test < 4:
+        best_t = None
+        best_d = None
+        for t_hold in range(t_min, t_max + 1):
+            this_dist = _evaluate_race(t_total, t_hold)
+            if best_t is None or this_dist > best_d:
+                best_t = t_hold
+                best_d = this_dist
+        return best_t
+    # n_to_test >= 4:
+    t_center = (t_min + t_max) // 2
+    t_pre_center = t_center - 1
+    if _evaluate_race(t_total, t_pre_center) < _evaluate_race(t_total, t_center):
+        # peak is to the right
+        return _find_peak(t_center, t_max, t_total=t_total)
+    # else:  # peak is to the left
+    return _find_peak(t_min, t_pre_center, t_total=t_total)
+
+
+def _binary_search(t_min: int, t_max: int, *, t_total: int, d_target: int, reverse: bool) -> int:
+    assert 0 <= t_min <= t_max <= t_total
+    d_start = _evaluate_race(t_total, t_min)
+    if d_start == d_target:
+        return t_min
+    d_end = _evaluate_race(t_total, t_max)
+    if d_end == d_target:
+        return t_max
+    if t_min + 1 == t_max:
+        if not reverse and (d_start < d_target < d_end):
+            return t_max
+        elif reverse and (d_end < d_target < d_start):
+            return t_min
+    if d_start == d_end and d_start != d_target:
+        raise ValueError("search edges are equal to each other, but not equal to the target")
+    assert (d_start < d_end) == (not reverse)
+    if (d_start < d_end and not (d_start <= d_target <= d_end)) or \
+            (d_end < d_start and not (d_end <= d_target <= d_start)):
+        raise ValueError("impossible search")
+    t_center = (t_min + t_max) // 2
+    d_center = _evaluate_race(t_total, t_center)
+    if d_center == d_target:
+        return t_center
+    if (d_target < d_center and not reverse) or (d_center < d_target and reverse):
+        return _binary_search(t_min, t_center, t_total=t_total, d_target=d_target, reverse=reverse)
+    elif (d_target < d_center and reverse) or (d_center < d_target and not reverse):
+        return _binary_search(t_center, t_max, t_total=t_total, d_target=d_target, reverse=reverse)
+    else:
+        raise RuntimeError("bad code 😅")
+
+
+def _count_ways_to_win(time_total: int, distance_to_beat: int) -> int:
+    t_best_possible = _find_peak(0, time_total, t_total=time_total)
+    if _evaluate_race(time_total, t_best_possible) <= distance_to_beat:
+        return 0
+    # binary searches to find the t-values that get the target d
+    t_low = _binary_search(0, t_best_possible, t_total=time_total, d_target=distance_to_beat, reverse=False)
+    assert _evaluate_race(time_total, t_low) >= distance_to_beat
+    t_high = _binary_search(t_best_possible, time_total, t_total=time_total, d_target=distance_to_beat, reverse=True)
+    assert _evaluate_race(time_total, t_high) >= distance_to_beat
+    # trim boundaries inward
+    while _evaluate_race(time_total, t_low) == distance_to_beat and t_low < t_high:
+        t_low += 1
+    while _evaluate_race(time_total, t_high) == distance_to_beat and t_low < t_high:
+        t_high -= 1
+    if t_low == t_high:
+        return int(_evaluate_race(time_total, t_low) > distance_to_beat)
+    return 1 + t_high - t_low
+
+
+def solve2(input_: Input) -> int:
+    time_total = int("".join(map(str, (t for t, _ in input_))))
+    distance_to_beat = int("".join(map(str, (d for _, d in input_))))
+    return _count_ways_to_win(time_total, distance_to_beat)
+
+
 def main():
     input_ = read_input()
-    answer = solve(input_)
+    answer = solve1(input_)
+    pprint(answer)
+    answer = solve2(input_)
     pprint(answer)
 
 
