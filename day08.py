b307fdccb40d73bda797fd34afbd1b7c6b9ec242
Ryan Echols <rkechols21@gmail.com>
2023-12-08 03:49:40 -0700

    day 8 part 2

--- day08.py
+++ day08.py
@@ -1,8 +1,9 @@
 import itertools
+import math
 import re
+from functools import reduce
 from pathlib import Path
 from pprint import pprint
-from typing import Literal
 
 Input = tuple[str, dict[str, tuple[str, str]]]
 
@@ -20,7 +21,7 @@ def read_input() -> Input:
     return directions, graph
 
 
-def _apply_direction(cur: str, direction: Literal["L", "R"], graph: dict[str, tuple[str, str]]) -> str:
+def _apply_direction(cur: str, direction: str, graph: dict[str, tuple[str, str]]) -> str:
     left, right = graph[cur]
     match direction:
         case "L":
@@ -41,49 +42,71 @@ def solve1(input_: Input) -> int:
             return i
 
 
-def _map_path_to_bools(path: list[str]) -> list[bool]:
-    return [
-        loc.endswith("Z")
-        for loc in path
-    ]
+def find_overlap_pos(pos1: int, n1: int, pos2: int, n2: int) -> tuple[int, int]:
+    while pos1 != pos2:
+        if pos1 < pos2:
+            n_hops = math.ceil((pos2 - pos1) / n1)
+            pos1 += n_hops * n1
+        else:  # pos1 > pos2
+            n_hops = math.ceil((pos1 - pos2) / n2)
+            pos2 += n_hops * n2
+    return pos1, math.lcm(n1, n2)
 
 
 def solve2(input_: Input) -> int:
     directions, graph = input_
-    assert len(directions) > 0
+    n = len(directions)
+    # analyze cyclical nature of movements
     cycles = {}
-    for start in graph.keys():
-        if not start.endswith("A"):
-            continue  # not a starter
-        path = [start]
-        cur = start
-        for direction in itertools.cycle(directions):
-            cur = _apply_direction(cur, direction, graph)
-            try:
-                loop_start = path.index(cur)
-            except ValueError:
-                path.append(cur)
-            else:
-                break
+    for start_loc in filter(lambda loc: loc.endswith("A"), graph.keys()):
+        states_seen = {}
+        z_sequence = []
+        cur = start_loc
+        for i, i_mod in enumerate(itertools.cycle(range(n))):
+            state = (cur, i_mod)
+            if state in states_seen:
+                break  # about to start looping
+            states_seen[state] = i
+            z_sequence.append(cur.endswith("Z"))
+            cur = _apply_direction(cur, directions[i_mod], graph)
         else:
             raise ValueError("empty list of directions")
-        intro, cycle = path[:loop_start], path[loop_start:]
-        cycles[start] = (intro, cycle)
-    pprint(cycles)
-    cycles_bools = {
-        start: (_map_path_to_bools(intro), _map_path_to_bools(cycle))
-        for start, (intro, cycle) in cycles.items()
-    }
-    pprint(cycles_bools)
-    has_z = {
-        start: (any(intro), any(cycle))
-        for start, (intro, cycle) in cycles_bools.items()
-    }
-    pprint(has_z)
-    for start, (intro, cycle) in cycles_bools.items():
-        if not any(itertools.chain(intro, cycle)):
-            raise ValueError(f"cycle starting from {start} did not have any True value")
-    print("😖")
+        i_start_cycle = states_seen[state]
+        intro, cycle = z_sequence[:i_start_cycle], z_sequence[i_start_cycle:]
+        cycles[start_loc] = (intro, cycle)
+    # check that the input is within the subset that this program can solve
+    for intro, cycle in cycles.values():
+        if (sum(intro), sum(cycle)) != (0, 1):  # no Z in intros, only 1 Z per cycle
+            # TODO: add a semi-smart brute-force as backup
+            raise RuntimeError("this code isn't ready to handle more complicated Z-patterns")
+    # convert from lists to integers
+    intro_lengths = {start_loc: len(intro) for start_loc, (intro, _) in cycles.items()}
+    longest_intro = max(intro_lengths.values())
+    cycle_lengths = {start_loc: len(cycle) for start_loc, (_, cycle) in cycles.items()}
+    z_positions_in_cycles = {start_loc: cycle.index(True) for start_loc, (_, cycle) in cycles.items()}
+    # shift the period window of each cycle (roll the cycle) so they all start at the end of the longest intro
+    for start_loc in z_positions_in_cycles:
+        shift = (longest_intro - intro_lengths[start_loc]) % cycle_lengths[start_loc]
+        z_positions_in_cycles[start_loc] -= shift
+    # "reduce" across `z_positions_in_cycles` using `find_overlap_pos`
+    overlap_pos = None
+    sync_period = None
+    for start_loc, z_pos in z_positions_in_cycles.items():
+        if overlap_pos is None:  # first
+            overlap_pos = z_pos
+            sync_period = cycle_lengths[start_loc]
+        else:  # 2nd and others
+            overlap_pos, sync_period = find_overlap_pos(
+                z_pos, cycle_lengths[start_loc],
+                overlap_pos, sync_period,
+            )
+    if overlap_pos is None or sync_period is None:
+        raise ValueError("no cycles to sync")
+    # fun fact:
+    assert sync_period == math.lcm(*cycle_lengths.values())
+    # adjust to account for intro steps
+    first_universal_overlap = longest_intro + overlap_pos
+    return first_universal_overlap
 
 
 def main():
