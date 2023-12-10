aed5f3a5ead0348a6e944b9550b2f6fd77ea71e1
Ryan Echols <rkechols21@gmail.com>
2023-12-10 04:38:08 -0700

    day 10 - cleanup

--- day10.py
+++ day10.py
@@ -1,30 +1,32 @@
 from enum import Enum
-import itertools
 from pathlib import Path
 from pprint import pprint
 from typing import Iterable, Self
 
 import numpy as np
 
-INPUT_FILE_PATH = Path("input.txt")
-
 Loc = tuple[int, int]
+PipeGrid = np.ndarray  # 2-dimensional array of single-char strings
+
 START = "S"
 EMPTY = "."
 
+INPUT_FILE_PATH = Path("input.txt")
 
-def read_input() -> np.ndarray:
-    with open(INPUT_FILE_PATH, "r", encoding="utf-8") as f:
-        return np.pad(np.array([
-            list(line.strip())
-            for line in f
-        ]), 1, constant_values=".")
+
+def _find_start(grid: PipeGrid) -> Loc:
+    for r, row in enumerate(grid):
+        for c, char in enumerate(row):
+            if char == START:
+                return r, c
+    else:
+        raise ValueError("start location not found")
 
 
 class Direction(Enum):
-    RIGHT = (0, 1)
     UP = (-1, 0)
     LEFT = (0, -1)
+    RIGHT = (0, 1)
     DOWN = (1, 0)
 
     def opposite(self) -> Self:
@@ -45,29 +47,62 @@ def shift(loc: Loc, direction: Direction) -> Loc:
     return loc[0] + r_shift, loc[1] + c_shift
 
 
-def symbol_to_directions(sym: str) -> tuple[Direction, Direction]:
-    if sym == "-":
-        return Direction.LEFT, Direction.RIGHT
-    if sym == "|":
-        return Direction.UP, Direction.DOWN
-    if sym == "F":
-        return Direction.RIGHT, Direction.DOWN
-    if sym == "7":
-        return Direction.LEFT, Direction.DOWN
-    if sym == "J":
-        return Direction.LEFT, Direction.UP
-    if sym == "L":
-        return Direction.RIGHT, Direction.UP
-    raise ValueError("symbol was not a tunnel shape")
+class Pipe(Enum):
+    UL = "J"
+    UR = "L"
+    UD = "|"
+    LR = "-"
+    LD = "7"
+    RD = "F"
+
+    def to_directions(self) -> tuple[Direction, Direction]:
+        dict_ = {
+            self.UL: (Direction.UP, Direction.LEFT),
+            self.UR: (Direction.UP, Direction.RIGHT),
+            self.UD: (Direction.UP, Direction.DOWN),
+            self.LR: (Direction.LEFT, Direction.RIGHT),
+            self.LD: (Direction.LEFT, Direction.DOWN),
+            self.RD: (Direction.RIGHT, Direction.DOWN),
+        }
+        d1, d2 = sorted(dict_[self], key=lambda d: d.value)
+        return d1, d2
+
+    @classmethod
+    def from_directions(cls, directions: tuple[Direction, Direction]) -> Self:
+        d1, d2 = sorted(directions, key=lambda d: d.value)
+        dict_ = {
+            (Direction.UP, Direction.LEFT): cls.UL,
+            (Direction.UP, Direction.RIGHT): cls.UR,
+            (Direction.UP, Direction.DOWN): cls.UD,
+            (Direction.LEFT, Direction.RIGHT): cls.LR,
+            (Direction.LEFT, Direction.DOWN): cls.LD,
+            (Direction.RIGHT, Direction.DOWN): cls.RD,
+        }
+        return dict_[(d1, d2)]
 
 
-def find_start(grid: np.ndarray) -> Loc:
-    for r, row in enumerate(grid):
-        for c, char in enumerate(row):
-            if char == START:
-                return r, c
-    else:
-        raise ValueError("start location not found")
+def read_input() -> tuple[PipeGrid, Loc]:
+    # read raw input
+    with open(INPUT_FILE_PATH, "r", encoding="utf-8") as f:
+        grid = np.array([
+            list(line.strip())
+            for line in f
+        ])
+    # pad for convenience
+    grid = np.pad(grid, 1, constant_values=EMPTY)
+    # find the start symbol and figure out what kind of pipe it is
+    start_loc = _find_start(grid)
+    start_directions = set()
+    for direction in Direction:
+        neighbor_loc = shift(start_loc, direction)
+        neighbor = grid[neighbor_loc]
+        if neighbor != EMPTY and direction.opposite() in Pipe(neighbor).to_directions():
+            start_directions.add(direction)
+    assert len(start_directions) == 2
+    start_pipe = Pipe.from_directions(start_directions)
+    # overwrite the start symbol with its actual pipe symbol
+    grid[start_loc] = start_pipe.value
+    return grid, start_loc
 
 
 def neighbors(loc: Loc) -> Iterable[Loc]:
@@ -75,81 +110,57 @@ def neighbors(loc: Loc) -> Iterable[Loc]:
         yield shift(loc, direction)
 
 
-def solve1(grid: np.ndarray) -> tuple[int, list[tuple[Loc, Direction]]]:
-    start = find_start(grid)
-    start_directions = set()
-    for direction in Direction:
-        neighbor_loc = shift(start, direction)
-        neighbor = grid[neighbor_loc]
-        if neighbor != EMPTY and direction.opposite() in symbol_to_directions(neighbor):
-            start_directions.add(direction)
-    if start_directions == {Direction.LEFT, Direction.UP}:
-        start_symbol = "J"
-    elif start_directions == {Direction.LEFT, Direction.DOWN}:
-        start_symbol = "7"
-    elif start_directions == {Direction.RIGHT, Direction.UP}:
-        start_symbol = "L"
-    elif start_directions == {Direction.RIGHT, Direction.DOWN}:
-        start_symbol = "F"
-    elif start_directions == {Direction.LEFT, Direction.RIGHT}:
-        start_symbol = "-"
-    elif start_directions == {Direction.UP, Direction.DOWN}:
-        start_symbol = "|"
-    else:
-        raise ValueError("🫠")
-    grid[start] = start_symbol
-    start_direction = next(iter(start_directions))
-    loop = [(start, start_direction)]
-    loc = shift(start, start_direction)
-    while loc != start:
-        for direction in symbol_to_directions(grid[loc]):
-            new_loc = shift(loc, direction)
-            if new_loc != loop[-1][0]:  # don't go backward
-                # figured out what direction to go from here; write it down and do it
-                loop.append((loc, direction))
-                loc = new_loc
-                break
-        else:
-            raise ValueError("idk")
+def solve1(grid: PipeGrid, start_loc: Loc) -> tuple[int, set[Loc]]:
+    loop_locs = {start_loc}
+    cur_locs = {start_loc}
+    while True:
+        cur_locs = {
+            shift(cur_loc, direction)
+            for cur_loc in cur_locs
+            for direction in Pipe(grid[cur_loc]).to_directions()
+        }
+        cur_locs -= loop_locs
+        loop_locs.update(cur_locs)
+        n_cur_locs = len(cur_locs)
+        assert 1 <= n_cur_locs <= 2
+        if n_cur_locs == 1:
+            break  # the two paths met
     # loop completed
-    loop_size = len(loop)
+    loop_size = len(loop_locs)
     half, remainder = divmod(loop_size, 2)
     assert remainder == 0
-    return half, loop
+    return half, loop_locs
 
 
-def print_grid(grid: np.ndarray):
-    print("\n".join("".join(row) for row in grid))
-
-
-def solve2(grid: np.ndarray, loop: list[tuple[Loc, Direction]]) -> int:
-    canvas = np.array([[" " for _ in range(grid.shape[1])] for _ in range(grid.shape[0])])
-    for loop_loc, _ in loop:
-        canvas[loop_loc] = grid[loop_loc]
-    loop_locs = {loop_loc for loop_loc, _ in loop}
+def solve2(grid: PipeGrid, loop_locs: set[Loc]) -> int:
     n_inside = 0
-    for search_start_loc in itertools.product(*(range(dim_size) for dim_size in grid.shape)):
-        if search_start_loc in loop_locs:
-            continue
-        n_crossings = 0
-        search_loc = search_start_loc
-        while (search_loc := shift(search_loc, Direction.UP))[0] > 0:
-            if search_loc in loop_locs:
-                symbol = grid[search_loc]
-                if symbol in "-J7":
-                    n_crossings += 1
-                elif symbol not in "|FL":
-                    raise ValueError(f"symbol {symbol!r} should not be part of the loop")
-        if n_crossings % 2 != 0:  # odd crossing; we were inside
-            n_inside += 1
+    is_loop = np.array([
+        [
+            (r, c) in loop_locs
+            for c in range(grid.shape[1])
+        ]
+        for r in range(grid.shape[0])
+    ])
+    is_inside = np.zeros(grid.shape[1], dtype=bool)
+    for row, is_loop_row in zip(grid, is_loop):
+        could_be_crossing = np.array([
+            Pipe(symbol) in (Pipe.UL, Pipe.LR, Pipe.LD)
+                if symbol != EMPTY
+                else False
+            for symbol in row
+        ])
+        is_crossing = np.logical_and(could_be_crossing, is_loop_row)
+        is_inside = np.where(is_crossing, np.logical_not(is_inside), is_inside)
+        is_inside_fully = np.logical_and(is_inside, np.logical_not(is_loop_row))
+        n_inside += np.count_nonzero(is_inside_fully)
     return n_inside
 
 
 def main():
-    input_ = read_input()
-    answer, loop = solve1(input_)
+    grid, start_loc = read_input()
+    answer, loop_locs = solve1(grid, start_loc)
     pprint(answer)
-    answer = solve2(input_, loop)
+    answer = solve2(grid, loop_locs)
     pprint(answer)
 
 
