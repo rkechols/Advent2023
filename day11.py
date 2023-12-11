e066fa75e3ddde970f89c146340f429c03c03585
Ryan Echols <rkechols21@gmail.com>
2023-12-10 22:50:53 -0700

    day 11 part 2

--- day11.py
+++ day11.py
@@ -1,8 +1,8 @@
-from pathlib import Path
 import itertools
+from pathlib import Path
 from pprint import pprint
+
 import numpy as np
-from typing import Any
 
 Input = np.ndarray  # feel free to change per-problem; whatever structure is easiest
 
@@ -19,43 +19,38 @@ def read_input() -> Input:
         ], dtype=bool)
 
 
-def expand_space(grid: np.ndarray) -> np.ndarray:
-    # rows
-    i = 0
-    while i < grid.shape[0]:
-        row = grid[i]
-        if np.count_nonzero(row) == 0:
-            row_duplicated = np.stack([row, row], axis=0)
-            grid = np.concatenate(
-                [grid[:i], row_duplicated, grid[(i + 1):]],
-                axis=0,
-            )
-            i += 2
-        else:
-            i += 1
-    # cols
-    j = 0
-    while j < grid.shape[1]:
-        col = grid[:, j]
-        if np.count_nonzero(col) == 0:
-            col_duplicated = np.stack([col, col], axis=1)
-            grid = np.concatenate(
-                [grid[:, :j], col_duplicated, grid[:, (j + 1):]],
-                axis=1,
-            )
-            j += 2
-        else:
-            j += 1
-    return grid
-
-
-def solve(grid: Input) -> int:
-    grid = expand_space(grid)
-    galaxy_locs = set()
+def grid_to_locs(grid: np.ndarray) -> list[tuple[int, int]]:
+    galaxy_locs = []
     for loc in itertools.product(range(grid.shape[0]), range(grid.shape[1])):
         if grid[loc]:
-            galaxy_locs.add(loc)
-    galaxy_locs = sorted(galaxy_locs)
+            galaxy_locs.append(loc)
+    return galaxy_locs
+
+
+def expand_space(locs: list[tuple[int, int]], *, expansion: int) -> list[tuple[int, int]]:
+    if expansion < 0:
+        raise ValueError("expansion undefined for negative values")
+    if expansion == 1:
+        return locs
+    # use numpy array for vectorized operations
+    # NOTE: integer overflow is possible, but numpy will give a RuntimeWarning if it does.
+    #    To avoid any possible overflow, stay with python built-in types like `list` and `int`
+    locs = np.array(locs, dtype=np.int64)  # shape (n, 2)
+    size_diff = expansion - 1
+    for axis in range(locs.shape[1]):
+        i = 0
+        while i < locs[:, axis].max():
+            if np.count_nonzero(locs[:, axis] == i) == 0:
+                locs[locs[:, axis] > i, axis] += size_diff
+                i += size_diff  # move cursor to last new index
+            i += 1  # move on
+    # convert output
+    return [tuple(loc) for loc in locs]
+
+
+def solve(grid: Input, *, expansion: int) -> int:
+    galaxy_locs = grid_to_locs(grid)
+    galaxy_locs = expand_space(galaxy_locs, expansion=expansion)
     total = 0
     for i, loc1 in enumerate(galaxy_locs):
         for loc2 in galaxy_locs[(i + 1):]:
@@ -68,7 +63,9 @@ def solve(grid: Input) -> int:
 
 def main():
     input_ = read_input()
-    answer = solve(input_)
+    answer = solve(input_, expansion=2)
+    pprint(answer)
+    answer = solve(input_, expansion=1_000_000)
     pprint(answer)
 
 
